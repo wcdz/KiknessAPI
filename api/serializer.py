1298809b63b9896ad51a4a00d4341b8fb56002b7
wcdz <william.chavez@vivirseguros.pe>
2024-08-29 16:19:30 -0500

    serializer urls add

---
+++ api/serializer.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from models import ViewEstudiantes
+
+class ViewEstudiantesSerializer(serializers.ModelSerializer):
+    model = ViewEstudiantes
+    fields = '__all__'
