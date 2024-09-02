18bf1340abf7bf2e2bf00dc61de45aa03d7e9c7b
wcdz <william.chavez@vivirseguros.pe>
2024-09-02 16:42:28 -0500

    alertas params add

--- api/services/alertas_service.py
+++ api/services/alertas_service.py
@@ -1,7 +1,23 @@
-from django.http.response import JsonResponse
+from django.http import JsonResponse
+from django.db.models import Q
 from ..models import Alertas
 
-
 def listado_alertas(request):
-    alertas = list(Alertas.objects.values())
+    estado = request.GET.get("estado", "").strip()
+    fecha_inicio = request.GET.get("fecha_inicio", "").strip()
+    fecha_fin = request.GET.get("fecha_fin", "").strip()
+    
+    alertas = Alertas.objects.all()
+    
+    if estado: 
+        alertas = alertas.filter(estado_alerta=estado)
+
+    filtros = Q()
+    if fecha_inicio:
+        filtros &= Q(fecha_alerta__gte=fecha_inicio)
+    if fecha_fin:
+        filtros &= Q(fecha_alerta__lte=fecha_fin)
+        
+    alertas = list(alertas.filter(filtros).values())
+    
     return JsonResponse(alertas, safe=False)
